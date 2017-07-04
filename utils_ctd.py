e0dc3f9b1eda039c5f8778e3531f6fc83e11de15
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2017-07-04 15:46:57 +0200

    fully functioning refactored utils_ctd module

--- utils_ctd.py
+++ utils_ctd.py
@@ -1,4 +1,16 @@
 def read_matlab(ctd_mat_file):
+    '''Read IMR matlab CTD transect file to python dictionary
+
+    Args
+    ----
+    ctd_mat_file: str
+        full path and name of IMR CTD Matlab file
+
+    Returns
+    -------
+    transects: OrderedDict
+        Dictionary of transects, each with corresponding CTD data
+    '''
     from collections import OrderedDict
     import scipy.io
 
@@ -25,7 +37,19 @@ def read_matlab(ctd_mat_file):
     return transects
 
 
-def calc_depth_date(d):
+def calc_depth_date(transect):
+    '''Cacluate the depth from pressure and convert matlab date to python
+
+    Args
+    ----
+    transect: dict
+        Transect dictionary with corresponding CTD data
+
+    Returns
+    -------
+    transect: dict
+        Input transect dictionary with depths and updated timestamps
+    '''
 
     def matlab2datetime(matlab_datenum):
         '''Convert Matlab datenum to Python datetime'''
@@ -40,48 +64,138 @@ def calc_depth_date(d):
     import gsw
     import numpy
 
-    # Remove prefix from columns of station dataframe
-    rm_prefix(d, 'KF')
-
-    n_depths, n_stations = d['date_up'].shape
-    d['depth'] = numpy.zeros((n_depths, n_stations), dtype=float)
-    d['date_up'] = d['date_up'].astype(object)
-    lats = numpy.meshgrid(d['lat'], numpy.ones(n_depths))[0]
+    n_depths, n_stations = transect['date_up'].shape
+    transect['depth'] = numpy.zeros((n_depths, n_stations), dtype=float)
+    transect['date_up'] = transect['date_up'].astype(object)
+    lats = numpy.meshgrid(transect['lat'], numpy.ones(n_depths))[0]
 
     for i in range(n_depths):
         for j in range(n_stations):
-            d['depth'][i][j] = gsw.z_from_p(d['press_up'][i][j], lats[i][j])
+            transect['depth'][i][j] = gsw.z_from_p(transect['press_up'][i][j],
+                                                   lats[i][j])
             try:
-                d['date_up'][i][j] = matlab2datetime(d['date_up'][i][j])
+                transect['date_up'][i][j] = matlab2datetime(transect['date_up'][i][j])
             except:
                 pass
-    return d
+
+    return transect
 
 
 def get_station_lonlats(transects):
+    '''Get latitude and longitude for each station in each transect
+
+    Args
+    ----
+    transects: OrderedDict
+        Dictionary of transects, each with corresponding CTD data
+
+    Returns
+    -------
+    data: pandas.DataFrame
+        Dataframe with transect name, latitude and longitude of each station
+    '''
     import pandas
 
     # Count number of lons/lats in all station dataframes
     n = 0
-    for d in transects.values():
-        n += len(d['lon'])
+    for transect in transects.values():
+        n += len(transect['lon'])
 
     # Create label, lon, lat dataframe
     data = pandas.DataFrame(index=range(n), columns=['id', 'lon', 'lat'])
 
     n0 = 0
-    for l, d in transects.items():
-        n1 = len(d['lon'])-1
-        data.ix[n0:n0+n1, 'lon'] = d['lon'].T
-        data.ix[n0:n0+n1, 'lat'] = d['lat'].T
-        data.ix[n0:n0+n1, 'id'] = l
+    for key, transect in transects.items():
+        n1 = len(transect['lon'])-1
+        data.ix[n0:n0+n1, 'lon'] = transect['lon'].T
+        data.ix[n0:n0+n1, 'lat'] = transect['lat'].T
+        data.ix[n0:n0+n1, 'id'] = key
         n0 = n1
 
     return data
 
 
+def find_nearest_station(lon, lat, transects):
+    '''Find the transect and lon/lat index of the station nearest to lon, lat
+
+    Args
+    ----
+    lon: float
+        Longitude from which the distance to the station locations are measured
+    lat: float
+        Latitude from which the distance to the station locations are measured
+    transects: OrderedDict
+        Dictionary of transects, each with corresponding CTD data
+
+    Returns
+    -------
+    transect_key: str
+        Key name of transect in `transects` with the station nearest to lon/lat
+    station_idx: int
+        Index of the transect data array for the station nearest to lon/lat
+    min_dist: float
+        Distance in meters betweeen lon/lat and the nearest station
+    '''
+    from collections import OrderedDict
+    import pyproj
+
+    g = pyproj.Geod(ellps='WGS84')
+
+    transect_key = ''
+    station_idx = ''
+    min_dist = 9e12
+    for k in transects.keys():
+        lons = transects[k]['lon']
+        lats = transects[k]['lat']
+
+        for i in range(len(lons)):
+            _, _, dist = g.inv(lon, lat, lons[i], lats[i])
+            if dist[0] < min_dist:
+                transect_key = k
+                station_idx = i
+                min_dist = dist[0]
+
+    return transect_key, station_idx, min_dist
+
+
+def calc_mean_salinity(transects, transect_key, station_idx, max_depth):
+    '''Calculate the mean salinity above a maximum depth at a station in
+    transects
+
+    Args
+    ----
+    transects: OrderedDict
+        Dictionary of transects, each with corresponding CTD data
+    transect_key: str
+        Key name of transect in `transects` with the station nearest to lon/lat
+    station_idx: int
+        Index of the transect data array for the station nearest to lon/lat
+
+    Returns
+    -------
+    mean_sal: float
+        Mean salinity above `max_depth` at specified station
+    '''
+    import numpy
+
+    # Cacluate mean salinity above max_depth
+    depths = transects[transect_key]['depth'][:, station_idx]
+    depth_mask = depths > -abs(max_depth)
+
+    sals = transects[transect_key]['sal_up'][:, station_idx]
+    mean_sal = numpy.mean(sals[depth_mask])
+
+    return mean_sal
+
+
 def plot_transect_data(transects):
-    '''Plot CTD transects from transects dictionary'''
+    '''Plot CTD transects from transects dictionary
+
+    Args
+    ----
+    transects: OrderedDict
+        Dictionary of transects, each with corresponding CTD data
+    '''
     import matplotlib.pyplot as plt
 
     from rjdtools.plot import maps
@@ -112,41 +226,6 @@ def plot_transect_data(transects):
     return None
 
 
-def find_nearest_station(lon, lat, transects):
-    from collections import OrderedDict
-    import pyproj
-
-    g = pyproj.Geod(ellps='WGS84')
-
-    nearest_key = ''
-    nearest_idx = ''
-    min_dist = 9e12
-    for k in transects.keys():
-        lons = transects[k]['lon']
-        lats = transects[k]['lat']
-
-        for i in range(len(lons)):
-            _, _, dist = g.inv(lon, lat, lons[i], lats[i])
-            if dist[0] < min_dist:
-                nearest_key = k
-                nearest_idx = i
-                min_dist = dist[0]
-
-    return nearest_key, nearest_idx, min_dist
-
-
-def calc_mean_salinity(transects, nearest_key, nearest_idx, max_depth):
-
-    # Cacluate mean salinity above max_depth
-    depths = transects[nearest_key]['depth'][:, nearest_idx]
-    depth_mask = depths > -abs(max_depth)
-
-    sals = transects[nearest_key]['sal_up'][:, nearest_idx]
-    mean_sal = numpy.mean(sals[depth_mask])
-
-    return mean_sal
-
-
 if __name__ == '__main__':
     import os
 
@@ -168,9 +247,9 @@ if __name__ == '__main__':
     transects = read_matlab(ctd_mat_file)
 
     # Find nearest station
-    nearest_key, nearest_idx, min_dist = find_nearest_station(lon, lat, transects)
+    transect_key, station_idx, min_dist = find_nearest_station(lon, lat, transects)
 
     # Cacluate mean salinity above 18m
-    mean_sal = calc_mean_salinity(transects, nearest_key, nearest_idx, max_depth):
+    mean_sal = calc_mean_salinity(transects, transect_key, station_idx, max_depth)
 
-    plot_transect_data(data)
+    plot_transect_data(transects)
