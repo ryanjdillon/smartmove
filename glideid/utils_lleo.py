4abdef62bd045fe32a6e3ddaeb65e3353b16afec
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2017-07-12 10:17:23 +0200

    updated utils_lleo for changes in pylleo, added args/returns to docstring

--- glideid/utils_lleo.py
+++ glideid/utils_lleo.py
@@ -1,66 +1,28 @@
 
-def speed_calibration_average(cal_fname, plot=False):
-    '''Cacluate the coefficients for the mean fit of calibrations
+def load_lleo(path_data_acc, file_cal_acc, file_cal_speed):
+    '''Load lleo data for calculating body condition
 
-    Notes
-    -----
-    `cal_fname` should contain three columns:
-    date,est_speed,count_average
-    2014-04-18,2.012,30
+    Args
+    ----
+    path_data_acc: str
+        Path to datalogger data files
+    file_cal_acc: str
+        Path to accelerometer calibration file to use for calibrating
+    file_cal_speed: str
+        Path to propeller calibration file to use for calibrating
+
+    Returns
+    -------
+    sensors: pandas.DataFrame
+        Dataframe with sensor data and calibrated sensor data
+    dt_a: float
+        Sampling rate of interpolated data
+    fs_a: float
+        Sampling frequency of interpolated data
     '''
-    import matplotlib.pyplot as plt
-    import numpy
-    import pandas
-
-    # Read calibration data
-    calibs = pandas.read_csv(cal_fname)
-
-    # Get unique dates to process fits for
-    dates = numpy.unique(calibs['date'])
-
-    # Create x data for samples and output array for y
-    n_samples = 1000
-    x = numpy.arange(n_samples)
-    fits = numpy.zeros((len(dates), n_samples), dtype=float)
-
-    # Calculate fit coefficients then store `n_samples number of samples
-    # Force intercept through zero (i.e. zero counts = zero speed)
-    # http://stackoverflow.com/a/9994484/943773
-    for i in range(len(dates)):
-        cal = calibs[calibs['date']==dates[i]]
-        xi = cal['count_average'].values[:, numpy.newaxis]
-        yi = cal['est_speed'].values
-        m, _, _, _ = numpy.linalg.lstsq(xi, yi)
-        fits[i, :] = m*x
-        # Add fit to plot if switch on
-        if plot:
-            plt.plot(x, fits[i,:], label='cal{}'.format(i))
-
-    # Calculate average of calibration samples
-    y_avg = numpy.mean(fits, axis=0)
-
-    # Add average fit to plot and show if switch on
-    if plot:
-        plt.plot(x, y_avg, label='avg')
-        plt.legend()
-        plt.show()
-
-    # Calculate fit coefficients for average samples
-    x_avg = x[:, numpy.newaxis]
-    m_avg, _, _, _ = numpy.linalg.lstsq(x_avg, y_avg)
-
-    return m_avg
-
-
-def load_lleo(path_data_acc, file_cal_acc, file_cal_speed, min_depth):
-    '''Load lleo data for calculating body condition'''
     import numpy
     import os
-
-    from pylleo.pylleo import lleoio, lleocal
-
-    import utils_prh
-    import utils_plot
+    from pylleo import lleoio, lleocal
     import yamlord
 
     # Parse tag model and id from directory/experiment name
@@ -72,7 +34,6 @@ def load_lleo(path_data_acc, file_cal_acc, file_cal_speed, min_depth):
     cal_dict = yamlord.read_yaml(file_cal_acc)
 
     # Verify sensor ID of data matches ID of CAL
-    # TODO add tag_id to pylleo cal, must enter manually now
     if cal_dict['tag_id'] != tag_id:
         raise SystemError('Data `tag_id` does not match calibration `tag_id`')
 
@@ -84,25 +45,16 @@ def load_lleo(path_data_acc, file_cal_acc, file_cal_speed, min_depth):
     sensors = lleoio.read_data(meta, path_data_acc, sample_f, overwrite=False)
 
     # Apply calibration to data
-    sensors['Ax_g'] = lleocal.apply_poly(sensors, cal_dict, 'acceleration_x')
-    sensors['Ay_g'] = lleocal.apply_poly(sensors, cal_dict, 'acceleration_y')
-    sensors['Az_g'] = lleocal.apply_poly(sensors, cal_dict, 'acceleration_z')
+    sensors = lleocal.calibrate_acc(sensors, cal_dict)
 
     # Calibrate propeller measurements to speed m/s^2
-    m_speed = utils_prh.speed_calibration_average(file_cal_speed)
-    sensors['speed'] = m_speed*sensors['propeller']
+    sensors = lleocal.calibrate_propeller(sensors, fil_cal_speed)
 
     # Linearly interpolate data
     sensors.interpolate('linear', inplace=True)
-
-    # TODO remove, leave diagnostic plot in parent routine?
-    exp_ind = range(len(sensors))
-    utils_plot.plot_swim_speed(exp_ind, sensors['speed'].values)
 
     # Get original sampling rates of accelerometer and depth sensors
     dt_a = float(meta['parameters']['acceleration_x']['Interval(Sec)'])
     fs_a = 1/dt_a
 
-    return sensors, dt_a, fs_a#A_g, depths, speed, temperature,
-
-
+    return sensors, dt_a, fs_a
