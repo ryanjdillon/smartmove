1d7aed3ccfd2e3b98c47ba88160e40d8ea87a1a3
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2017-08-08 21:30:39 +0200

    renamed sensors to tag, updates to docstrings

--- smartmove/glideid/utils_lleo.py
+++ smartmove/glideid/utils_lleo.py
@@ -1,19 +1,19 @@
 
-def load_lleo(path_data_acc, file_cal_acc, file_cal_speed):
+def load_lleo(path_data_tag, file_cal_acc, file_cal_prop):
     '''Load lleo data for calculating body condition
 
     Args
     ----
-    path_data_acc: str
+    path_data_tag: str
         Path to datalogger data files
     file_cal_acc: str
         Path to accelerometer calibration file to use for calibrating
-    file_cal_speed: str
+    file_cal_prop: str
         Path to propeller calibration file to use for calibrating
 
     Returns
     -------
-    sensors: pandas.DataFrame
+    tag: pandas.DataFrame
         Dataframe with sensor data and calibrated sensor data
     dt_a: float
         Sampling rate of interpolated data
@@ -26,7 +26,7 @@ def load_lleo(path_data_acc, file_cal_acc, file_cal_speed):
     import yamlord
 
     # Parse tag model and id from directory/experiment name
-    experiment_id = os.path.split(path_data_acc)[1].replace('-','')
+    experiment_id = os.path.split(path_data_tag)[1].replace('-','')
     tag_model = experiment_id.split('_')[1]
     tag_id = int(experiment_id.split('_')[2])
 
@@ -39,23 +39,23 @@ def load_lleo(path_data_acc, file_cal_acc, file_cal_speed):
                           '`tag_id` ({})'.format(tag_id, cal_dict['tag_id']))
 
     # Load meta data
-    meta = lleoio.read_meta(path_data_acc, tag_model, tag_id)
+    meta = lleoio.read_meta(path_data_tag, tag_model, tag_id)
 
     # Load data
     sample_f  = 1
-    sensors = lleoio.read_data(meta, path_data_acc, sample_f, overwrite=False)
+    tag = lleoio.read_data(meta, path_data_tag, sample_f, overwrite=False)
 
     # Apply calibration to data
-    sensors = lleocal.calibrate_acc(sensors, cal_dict)
+    tag = lleocal.calibrate_acc(tag, cal_dict)
 
     # Calibrate propeller measurements to speed m/s^2
-    sensors = lleocal.calibrate_propeller(sensors, file_cal_speed)
+    tag = lleocal.calibrate_propeller(tag, file_cal_prop)
 
     # Linearly interpolate data
-    sensors.interpolate('linear', inplace=True)
+    tag.interpolate('linear', inplace=True)
 
     # Get original sampling rates of accelerometer and depth sensors
     dt_a = float(meta['parameters']['acceleration_x']['Interval(Sec)'])
     fs_a = 1/dt_a
 
-    return sensors, dt_a, fs_a
+    return tag, dt_a, fs_a
