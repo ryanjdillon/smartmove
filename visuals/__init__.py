7327bbc95766eab188243ade3a17f5a6635c57ca
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2017-08-21 10:41:21 +0200

    removed make_all from visuals/__init__.py, added submodule imports

--- visuals/__init__.py
+++ visuals/__init__.py
@@ -1,9 +1,6 @@
-
-def make(path_project, path_analysis):
-    from . import figures
-    from . import tables
-
-    figures.make_all(path_project, path_analysis)
-    tables.make_all(path_project, path_analysis)
-
-    return None
+from . import dataexplore
+from . import figures
+from . import latex
+from . import table_attributes
+from . import tables
+from . import utils
