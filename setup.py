d2b0e1dfefbc0f5cb8f695cb0101b28372bce763
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2017-09-21 22:00:05 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='smartmove',
+    version='0.1',
+    description=('Utilities for working with biotelemetry and datalogger '
+                 'data in Python'),
+    long_description=long_description,
+    author='Ryan J. Dillon',
+    author_email='ryanjamesdillon@gmail.com',
+    url='https://github.com/ryanjdillon/smartmove',
+    download_url='https://github.com/ryanjdillon/smartmove/archive/0.1.tar.gz',
+    license='MIT',
+    packages=['smartmove'],
+    install_requires=[
+        'scipy==0.19.1',
+        'seaborn==0.8',
+        '#sklearn==0.0',
+        'scikit-learn==0.18.2',
+        'theanets==0.7.3',
+        'theano==0.9.0',
+        'pandas==0.20.3',
+        'gsw==3.1.1',
+        'numpy==1.13.1',
+        'matplotlib==2.0.2',
+        'yamlord==0.4',
+        'pylleo==0.1',
+        'pyotelem==0.1',],
+    include_package_data=True,
+    keywords=['datalogger','accelerometer','biotelemetry'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.5'],
+    zip_safe=False,
+    )
