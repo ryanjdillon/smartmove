214016e5a687fe3537e01d91e6990ea82e8c6a14
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2017-10-17 14:19:26 +0200

    changed suprocess to Popen with stdout readout and added nameref, hyperref packages

--- smartmove/visuals/latex/utils.py
+++ smartmove/visuals/latex/utils.py
@@ -7,7 +7,7 @@ def compile_latex(path, fname, out_ext=None, dpi=300):
     from contextlib import contextmanager
     from PIL import Image
     import os
-    import subprocess
+    from subprocess import Popen, PIPE, CalledProcessError
 
     @contextmanager
     def cd(newdir):
@@ -31,18 +31,20 @@ def compile_latex(path, fname, out_ext=None, dpi=300):
                 r'\usepackage{mathtools}'
                 r'\usepackage{gensymb}'
                 r'\usepackage{array}'
+                r'\usepackage{nameref, hyperref}'
                 r'\usepackage[usenames, dvipsnames]{color}'
                 r'\begin{document}'
                 r'\pagenumbering{gobble}'
                 r'\input{'+fname+'}'
                 r'\end{document}')
                ]
-        subprocess.run(cmd, stdout=subprocess.PIPE)
 
-        # Crop whitespace from table
-        #pdfname = 'article.pdf'
-        #cmd = ['pdfcrop', pdfname, pdfname]
-        #subprocess.run(cmd, stdout=subprocess.PIPE)
+        with Popen(cmd, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
+            for line in p.stdout:
+                print(line, end='') # process line here
+
+        if p.returncode != 0:
+            raise CalledProcessError(p.returncode, p.args)
 
         # Rename latex output to match filename
         os.renames('article.pdf', fname+'.pdf')
